cb16b5b879be52c4bff5d68ed235362a4c8b3816
teseo <sol@javiermellado.com>
2019-02-22 17:12:10 +0000

    autocomplete integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import autoBind from "react-autobind";
+import SearchBar from "./components/SearchBar";
+import styles from "./SearchBar.module.css";
+import words from "./words.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      suggestions: []
+    };
+
+    autoBind(this, "handleChange", "handleClear", "handleSelection");
+  }
+
+  handleClear() {
+    this.setState({
+      suggestions: []
+    });
+  }
+
+  handleChange(input) {
+    this.setState({
+      suggestions: words.filter(word => word.startsWith(input))
+    });
+  }
+
+  handleSelection(value) {
+    if (value) {
+      console.info(`Selected "${value}"`);
+    }
+  }
+
+  handleSearch(value) {
+    if (value) {
+      console.info(`Searching "${value}"`);
+    }
+  }
+
+  suggestionRenderer(suggestion, searchTerm) {
+    return (
+      <span>
+        <span>{searchTerm}</span>
+        <strong>{suggestion.substr(searchTerm.length)}</strong>
+      </span>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <SearchBar
+          autoFocus
+          renderClearButton
+          renderSearchButton
+          placeholder="select an SAT word"
+          onChange={this.handleChange}
+          onClear={this.handleClear}
+          onSelection={this.handleSelection}
+          onSearch={this.handleSearch}
+          suggestions={this.state.suggestions}
+          suggestionRenderer={this.suggestionRenderer}
+          styles={styles}
+        />
       </div>
     );
   }
