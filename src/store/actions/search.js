b8602a53879b4356f3304912c067b9f46876c117
teseo <sol@javiermellado.com>
2019-02-22 20:20:24 +0000

    integrate redux

---
+++ src/store/actions/search.js
@@ -0,0 +1,29 @@
+import ApiService from "../../utils";
+import {
+  REMOTE_SEARCH_FULFILLED,
+  REMOTE_SEARCH_START
+} from "./types";
+
+export const performRemoteSearch = () => async dispatch => {
+  dispatch({
+    type: REMOTE_SEARCH_START
+  });
+  const filters = {
+    TreatmentLocation: "S314H",
+    Month: "201812"
+  };
+  const limit = 138;
+  const offset = 0;
+  const nhsData = await ApiService.performSearch(
+    filters,
+    offset,
+    limit
+  );
+
+  dispatch({
+    type: REMOTE_SEARCH_FULFILLED,
+    payload: {
+      records: nhsData.result.records
+    }
+  });
+};
---
+++ src/store/reducers/search.js
@@ -0,0 +1,19 @@
+import {
+  REMOTE_SEARCH_FULFILLED
+} from "../actions/types";
+
+export const initialState = {
+  records: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case REMOTE_SEARCH_FULFILLED:
+      return {
+        ...state,
+        records: action.payload.records
+      };
+    default:
+      return state;
+  }
+}
